91bbb93a68b07a7d64a212d138c25777d06c44ed
Mayssa Zennou <mayssa.zn.11@gmail.com>
2026-08-31 17:28:53 +0100

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num: nums){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1 );
+        }
+
+        List<Map.Entry<Integer, Integer>> entries = new ArrayList<>(freqMap.entrySet());
+        entries.sort((a, b) -> b.getValue() - a.getValue());
+        
+        int[] result = new int[k];
+        for (int i=0; i<k; i++){
+            result[i] = entries.get(i).getKey();
+        }
+
+        return result;
+    }
+}
