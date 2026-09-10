6b99c8795bbc900a9740bc041ac033f5d71c5ab1
Mayssa Zennou <mayssa.zn.11@gmail.com>
2026-09-10 23:43:44 +0100

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+
+        // selection sort
+
+        int temp;
+        for (int i=0; i<nums.length; i++) {
+            // select minimum element and places it at the correct position at each pass
+            for (int j=i+1; j<nums.length; j++) {
+                if (nums[i]>nums[j]){
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        
+        return nums;
+    }
+}
